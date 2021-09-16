cc0a136576aef397038f00327b40d444e2044051
Oscar Mejia <oscar.ahuatl@gmail.com>
2021-09-15 22:54:20 -0500

    Todas las secciones ya estan listas

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ def root():
 if __name__=='__main__':
     app.run(debug=True)
 
+# TODO Cambiar el Favicon
+# TODO Agregar a la topbar ligas a mi educación e skills
+# TODO agregar las cartas de recomendación a "clients are saying
+# TODO cambiar el apartado What can I do
+# TODO pensar si quiero conservar el Portafolio
+# TODO Quitar el objeto de configuración de colores.
+
+
